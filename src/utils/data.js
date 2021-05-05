c5ccfd234ed6a6d18f08bd16a62028ed19ee67f7
Sergey Moskvitin <moskvitin.sergei@yandex.ru>
2021-05-05 21:08:00 +0300

    first 3

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,4 @@
 const API_BASE = 'https://api.moskvitins.movies.nomoredomains.club/';
-// const API_BASE = 'http://localhost:3000/';
 const API_MOVIES = 'https://api.nomoreparties.co/beatfilm-movies';
 
 module.exports = {
